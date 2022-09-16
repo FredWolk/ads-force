8b03c523856ff777e78b5633272da3a34f71f8ca
AlexandrKruglenko <fredwolk6@gmail.com>
2022-09-16 15:27:03 +0300

    Настройка проекта

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -13,6 +13,7 @@ return [
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'request' => [
+            'baseUrl' => '',
             'csrfParam' => '_csrf-frontend',
         ],
         'user' => [
@@ -36,14 +37,18 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
+            'rules' =>
+            [
+                //['class' => 'frontend\components\ClassUrlRule'],
+                '' => 'site/index',
+
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
