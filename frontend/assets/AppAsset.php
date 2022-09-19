01572c4eca4aa39100b2b7ccc2056181e86c20ab
Silver <gramakov27@gmail.com>
2022-09-19 13:00:35 +0300

    хедер + футтер

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,6 +13,9 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/header.css',
+        'css/footer.css',
+        'css/reset.css',
     ];
     public $js = [];
     public $depends = [
