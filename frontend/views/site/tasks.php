c7e4ae64eb690b8a554208dd18fdfba1d13abe5f
venegretik <don.artur16084353@gmail.com>
2022-09-21 20:29:38 +0300

    filter

--- frontend/views/site/tasks.php
+++ frontend/views/site/tasks.php
@@ -11,9 +11,33 @@ $this->registerCssFile(Url::to(['css/tasks.css']), ['depends' => ['frontend\asse
 $this->registerCssFile(Url::to(['css/component-css/filter.css']), ['depends' => ['frontend\assets\AppAsset']]);
 AppAsset::register($this);
 ?>
-<section class="filter-full">
-    <div class="title-categories">
-        <h2>Категория</h2>
-        <p>Сбросить</p>
-    </div>
-</section>
+<div class="container-index">
+    <section class="filter-full">
+        <div class="title-categories">
+            <h2 class="Font-size18">Категория</h2>
+            <p class="Font-size13">Сбросить</p>
+        </div>
+        <div class="categories-select">
+            <div class="categories-select-top">
+                <b class="Font-size18">Интернет продвижение и реклама</b>
+                <img src="<?= Url::to(['img/tasks/arrow-filter.svg']) ?>" alt="">
+            </div>
+            <div class="categories-select-bottom">
+                <ul>
+                    <li>
+                        <label class="custom-checkbox">
+                            <input type="checkbox" value="value-1">
+                            <span class="Font-size13">Таргетированная реклама</span>
+                        </label>
+                    </li>
+                    <li>
+                        <label class="custom-checkbox">
+                            <input type="checkbox" value="value-1">
+                            <span class="Font-size13">Интернет-маркетинг</span>
+                        </label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</div>
