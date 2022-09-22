1bdea47aecc50e83d23018a71896c0f0330b53e3
venegretik <don.artur16084353@gmail.com>
2022-09-22 18:17:59 +0300

    slider-range

--- frontend/views/site/tasks.php
+++ frontend/views/site/tasks.php
@@ -10,7 +10,8 @@ $this->title = 'My.Force';
 $this->registerCssFile(Url::to(['css/tasks.css']), ['depends' => ['frontend\assets\AppAsset']]);
 $this->registerCssFile(Url::to(['css/component-css/filter.css']), ['depends' => ['frontend\assets\AppAsset']]);
 $this->registerCssFile(Url::to(['css/component-css/task-item.css']), ['depends' => ['frontend\assets\AppAsset']]);
-$this->registerJsFile(Url::to(['js/slick.min.js']), ['depends' => JqueryAsset::class]);
+$this->registerCssFile(Url::to(['css/component-css/slider-rage.css']), ['depends' => ['frontend\assets\AppAsset']]);
+$this->registerJsFile(Url::to(['js/slider-range.js']), ['depends' => JqueryAsset::class]);
 $js = <<< JS
 let statusFilter = false
     $('.filter-item .categories-select-top').click(function (e) {
@@ -54,9 +55,38 @@ AppAsset::register($this);
     <div class="tasks-content-main">
         <!--filter-->
         <section class="filter-full">
+            <div class="title-remuneration">
+                <h2 class="Font-size18">Вознаграждение</h2>
+            </div>
+            <div class="remuneration">
+                <div class="remuneration-minmax">
+                    <div class="remuneration-minmax-item">
+                        <p>От 0</p>
+                        <img src="<?= Url::to(['img/tasks/rub.svg']) ?>" alt="">
+                    </div>
+                    <div class="remuneration-minmax-item">
+                        <p>До 100.000</p>
+                        <img src="<?= Url::to(['img/tasks/rub.svg']) ?>" alt="">
+                    </div>
+                </div>
+                <div class="range_container">
+                    <div class="sliders_control">
+                        <input id="fromSlider" type="range" value="0" min="0" max="100000" />
+                        <input id="toSlider" type="range" value="100000" min="0" max="100000" />
+                    </div>
+                    <div class="form_control">
+                        <div class="form_control_container">
+                            <input class="form_control_container__time__input" type="number" id="fromInput" value="0" min="0" max="100000" />
+                        </div>
+                        <div class="form_control_container">
+                            <input class="form_control_container__time__input" type="number" id="toInput" value="100000" min="0" max="100000" />
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="title-categories">
                 <h2 class="Font-size18">Категория</h2>
-                <p class="Font-size13">Сбросить</p>
+                <a href="" class="Font-size13">Сбросить</a>
             </div>
             <div class="categories-select">
                 <div class="filter-item">
@@ -105,51 +135,56 @@ AppAsset::register($this);
                         </ul>
                     </div>
                 </div>
+            </div>
+            <div class="filter-show-more">
+                <a href="" class="Font-size18">Ещё +</a>
             </div>
         </section>
         <section class="task-list">
             <div class="tasks">
-                <div class="task-item">
-                    <div class="filter-task-item">
-                        <div class="filter-task-item-main">
-                            <div class="status-task">
-                                <p>Свободен</p>
+                <a href="">
+                    <div class="task-item">
+                        <div class="filter-task-item">
+                            <div class="filter-task-item-main">
+                                <div class="status-task">
+                                    <p>Свободен</p>
+                                </div>
+                                <div class="filter-view filter-task-items">
+                                    <img src="<?= Url::to(['img/tasks/view.svg']) ?>" alt="">
+                                    <p>22</p>
+                                </div>
+                                <div class="filter-view filter-task-items">
+                                    <img src="<?= Url::to(['img/tasks/human-icon.svg']) ?>" alt="">
+                                    <p>12</p>
+                                </div>
+                                <div class="filter-view filter-task-items">
+                                    <img src="<?= Url::to(['img/tasks/summ.svg']) ?>" alt="">
+                                    <p>Договорная</p>
+                                </div>
                             </div>
-                            <div class="filter-view filter-task-items">
-                                <img src="<?= Url::to(['img/tasks/view.svg']) ?>" alt="">
-                                <p>22</p>
-                            </div>
-                            <div class="filter-view filter-task-items">
-                                <img src="<?= Url::to(['img/tasks/view.svg']) ?>" alt="">
-                                <p>12</p>
-                            </div>
-                            <div class="filter-view filter-task-items">
-                                <img src="<?= Url::to(['img/tasks/view.svg']) ?>" alt="">
-                                <p>Договорная</p>
+                            <div class="right-item-filter">
+                                <p>21 сентября</p>
                             </div>
                         </div>
-                        <div class="right-item-filter">
-                            <p>21 сентября</p>
+                        <div class="task-title">
+                            <h2 class="Font-size24">Настройка рекламы ВКонтакте</h2>
+                        </div>
+                        <div class="task-text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Nunc vulputate libero et velit interdum, ac aliq... Подробнее</p>
+                        </div>
+                        <div class="task-tag-list">
+                            <div class="task-tag-item">
+                                <p>#тег</p>
+                            </div>
+                            <div class="task-tag-item">
+                                <p>#тег</p>
+                            </div>
+                            <div class="task-tag-item">
+                                <p>#тег</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="task-title">
-                        <h2 class="Font-size24">Настройка рекламы ВКонтакте</h2>
-                    </div>
-                    <div class="task-text">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Nunc vulputate libero et velit interdum, ac aliq... Подробнее</p>
-                    </div>
-                    <div class="task-tag-list">
-                        <div class="task-tag-item">
-                            <p>#тег</p>
-                        </div>
-                        <div class="task-tag-item">
-                            <p>#тег</p>
-                        </div>
-                        <div class="task-tag-item">
-                            <p>#тег</p>
-                        </div>
-                    </div>
-                </div>
+                </a>
             </div>
         </section>
     </div>
